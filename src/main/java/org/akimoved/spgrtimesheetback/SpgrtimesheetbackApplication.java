9317cc57cbf9e9ac2645640257d64cec53b3fd3f
Egor Akimov <akimoved@gmail.com>
2024-04-20 22:54:54 +0400

    initialize spring boot

---
+++ src/main/java/org/akimoved/spgrtimesheetback/SpgrtimesheetbackApplication.java
@@ -0,0 +1,13 @@
+package org.akimoved.spgrtimesheetback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpgrtimesheetbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpgrtimesheetbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/akimoved/spgrtimesheetback/SpgrtimesheetbackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.akimoved.spgrtimesheetback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpgrtimesheetbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
