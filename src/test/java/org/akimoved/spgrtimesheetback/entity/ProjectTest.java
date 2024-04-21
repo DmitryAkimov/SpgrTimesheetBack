d53a18c45b1e4895feda39ef143e1c6638c6ab1f
Egor Akimov <akimoved@gmail.com>
2024-04-21 15:38:14 +0400

    test add test class for Project

---
+++ src/test/java/org/akimoved/spgrtimesheetback/entity/ProjectTest.java
@@ -0,0 +1,11 @@
+package org.akimoved.spgrtimesheetback.entity;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("testing Project")
+class ProjectTest {
+
+}
