003ad2ea69cfe546fea39cef7c2f2a66e5db9418
Egor Akimov <akimoved@gmail.com>
2024-05-26 18:44:52 +0400

    feat war packaging and tomcat environment

--- src/main/java/org/akimoved/spgrtimesheetback/SpgrtimesheetbackApplication.java
+++ src/main/java/org/akimoved/spgrtimesheetback/SpgrtimesheetbackApplication.java
@@ -2,10 +2,13 @@ package org.akimoved.spgrtimesheetback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import jakarta.annotation.Resource;
 
 @SpringBootApplication
-public class SpgrtimesheetbackApplication {
-
+public class SpgrtimesheetbackApplication extends SpringBootServletInitializer {
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpgrtimesheetbackApplication.class, args);
 	}
